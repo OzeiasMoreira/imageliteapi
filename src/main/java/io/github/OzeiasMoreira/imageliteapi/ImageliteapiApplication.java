c25f9c7bb1f58197b4c3e2954a211ad43e8240fb
OzeiasMoreira <OzMoreira17@gmail.com>
2025-09-11 13:19:49 -0300

    Mapeamento JPA para entidade Image

--- src/main/java/io/github/OzeiasMoreira/imageliteapi/ImageliteapiApplication.java
+++ src/main/java/io/github/OzeiasMoreira/imageliteapi/ImageliteapiApplication.java
@@ -1,9 +1,12 @@
 package io.github.OzeiasMoreira.imageliteapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ImageliteapiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/io/github/OzeiasMoreira/imageliteapi/domain/enums/ImageExtension.java
@@ -0,0 +1,8 @@
+package io.github.OzeiasMoreira.imageliteapi.domain.enums;
+
+public enum ImageExtension {
+    PNG,
+    JPG,
+    JPEG,
+    GIF,
+}
