52fe93a96dbf2cdc387919c73a422b2bb931c8ef
Ozeias <ozmoreira17@gmail.com>
2025-09-20 14:12:30 -0300

    Adicionando capacidade de executar Specifications no repositorio de imagens

--- src/main/java/io/github/OzeiasMoreira/imageliteapi/infra/repository/ImageRepository.java
+++ src/main/java/io/github/OzeiasMoreira/imageliteapi/infra/repository/ImageRepository.java
@@ -1,8 +1,16 @@
 package io.github.OzeiasMoreira.imageliteapi.infra.repository;
 
 import io.github.OzeiasMoreira.imageliteapi.domain.entity.Image;
+import io.github.OzeiasMoreira.imageliteapi.domain.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query) {
+        return findAll();
+    }
 
 }
